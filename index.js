20d65951a462899a0c5f0fe349c3d829e87edecf
Aayush Laddha <aayushladdha001@gmail.com>
2025-06-30 17:33:49 +0530

    .

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import fetch from 'node-fetch';
 import 'dotenv/config';
+import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
 
 const app = express();
 const port = 3000;
